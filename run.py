e0146cd6d68f14658f9a49702b10bbf76801afbb
bek <bekrye@gmail.com>
2023-06-13 15:53:52 +0200

    Add initial function structure

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class BattleShip:
+    def __init__():
+        pass
+
+    def game_home_page():
+        pass
+
+    def game_board():
+        pass
+
+    def board_size():
+        pass
+
+    def ship_numbers():
+        pass
+
+    def validate_input():
+        pass
+
+    def guess_ship_location():
+        pass
+
+    def create_battle_ships():
+        pass
+
+    def count_hits():
+        pass
+
+    def game_over_message():
+        pass
+
+    def is_game_over():
+        pass
+
+    def play():
+        pass
+
+    def new_game():
+        pass
+
