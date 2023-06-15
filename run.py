06eda98350d8b9764ee420480bf80fdf8a25545b
bek <bekrye@gmail.com>
2023-06-15 13:47:34 +0200

    Add print game board method

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from random import randint
 
-class BattleShip:
+class BattleShipGame:
     def __init__(self):
         self.board_size = 0
         self.player_score = 0
@@ -13,17 +13,22 @@ class BattleShip:
         self.computer_guessed_locations = []
 
 
-    def game_home_page():
-        pass
-
     def board_size():
         pass
 
     def ship_numbers():
         pass
 
-    def print_game_board():
-        pass
+    def print_game_board(self, board):
+        """
+        Prints the game board.
+
+        Args:
+            board (list): The game board to print.
+        """
+        print(' ' + ' '.join([str(i) for i in range(1, self.board_size + 1)]))
+        for i, row in enumerate(board):
+            print(str(i + 1) + '|' + '|'.join(row) + '|')
 
     def validate_input():
         pass
@@ -46,6 +51,17 @@ class BattleShip:
     def play():
         pass
 
-    def new_game():
+    def computer_guess():
         pass
 
+    def play():
+        pass
+
+
+    def game_home_page(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = BattleShipGame()
+    game.game_home_page()
