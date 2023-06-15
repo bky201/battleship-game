d36a57aec522b0529c65550a89badf4fe59d2613
bek <bekrye@gmail.com>
2023-06-15 13:27:26 +0200

    Add constructor method

--- run.py
+++ run.py
@@ -1,19 +1,28 @@
 from random import randint
 
 class BattleShip:
-    def __init__():
-        pass
+    def __init__(self):
+        self.board_size = 0
+        self.player_score = 0
+        self.computer_score = 0
+        self.Hidden_Pattern_Player = []
+        self.Hidden_Pattern_Computer = []
+        self.Guess_Pattern_Player = []
+        self.Guess_Pattern_Computer = []
+        self.player_guessed_locations = []
+        self.computer_guessed_locations = []
+
 
     def game_home_page():
-        pass
-
-    def game_board():
         pass
 
     def board_size():
         pass
 
     def ship_numbers():
+        pass
+
+    def print_game_board():
         pass
 
     def validate_input():
